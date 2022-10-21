9591ff4d0af3e548b90e6dc73a4b2f0f0c9a6809
vikasini4 <96987281+vikasini4@users.noreply.github.com>
2022-10-21 16:41:20 +0100

    Add files via upload

---
+++ links.py
@@ -0,0 +1,48 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import requests
+from bs4 import BeautifulSoup
+import time
+from selenium.webdriver.common.keys import Keys
+
+class webscraper():
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+    
+    def search(self, query): 
+        self.driver.get(f"https://www.google.com/search?q={query}&tbm=isch")
+      
+    def accept_form(self):
+        accept_button = self.driver.find_element(
+            By.XPATH, "//button[@aria-label='Accept all']")
+
+        accept_button.click()   
+        #print(accept_button)
+
+    def scroll_to_bottom(self):
+         print('\nScrolling to the bottom of the page right now, then clicking on \'next page\'.')
+         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+               
+    def get_all_links(self, query):
+        self.search(query)
+        self.accept_form()
+        container = self.driver.find_elements(By.XPATH, '//*[@id="islrg"]/div[1]/div') 
+        
+       
+        for element in container:
+           
+           a_tag = element.find_element(By.XPATH,'./a[1]')
+           a_tag.send_keys(Keys.ENTER)
+           #time.sleep(3)
+           link = a_tag.get_attribute('href')
+           print(link)
+    
+    
+scraper = webscraper()
+query = "radhakrishna"
+scraper.scroll_to_bottom()
+scraper.get_all_links(query)
+
+
+ 
---
+++ main.py
@@ -0,0 +1,4 @@
+from links import webscraper
+
+if __name__ == "__main__":
+    webscraper()
