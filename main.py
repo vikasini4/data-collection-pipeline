0c001a091aaf7a5b9bfb74ce89ab6129e0572453
vikasini4 <96987281+vikasini4@users.noreply.github.com>
2022-11-11 22:24:18 +0000

    Update and rename main.py to main1.py

--- main.py
+++
@@ -1,4 +0,0 @@
-from links import webscraper
-
-if __name__ == "__main__":
-    webscraper()
---
+++ main1.py
@@ -0,0 +1,7 @@
+import unittest
+
+from test_rk import webscraper
+
+if __name__ == '__main__':
+   
+    unittest.main(webscraper)  
