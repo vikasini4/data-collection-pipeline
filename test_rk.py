c3925d285620d63a6bf7932b476e7c2749c7f6da
vikasini4 <96987281+vikasini4@users.noreply.github.com>
2022-11-16 23:02:42 +0000

    Update test_rk.py

--- test_rk.py
+++ test_rk.py
@@ -1,21 +1,57 @@
-from radhakrishna import webscraper
+from rk import webscraper
 import unittest
+import os
 
 
-class TestwebscraperTestCase(unittest.TestCase):
+class webscraperTestCase(unittest.TestCase):
     def setUp(self):
         self.scraper = webscraper()
-        self.query = "radhakrishna"
         
     def test__init__(self):
        pass
-
-    def test_get_all_images(self, query):
-        self.search(query)
-        self.accept_form()
-        self.scroll_to_bottom()
-        self.get_all_links(query)
     
-    def tearDown(self):
-        self.scraper.driver.quit()
-        del self.scraper
+    def test_search(self): 
+        self.scraper.search()
+        url='https://www.google.com/search?q=radhakrishna&tbm=isch'
+        self.assertAlmostEqual(len(url),53)
+
+    def test_accept_form(self):
+        actual = self.scraper.accept_form()
+
+        try: 
+            expected1 = 'Cookies accepted'
+            self.assertEqual(actual,expected1)
+            print('Accept Cookies button correctly used')
+        except:
+            expected2 = 'No cookies to accept'
+            self.assertEqual(actual,expected2)
+            print('No Cookies button found')
+
+    def test_scroll_to_bottom(self):
+        self.scraper.accept_form()
+        self.scraper.scroll_to_bottom()
+        height1 = self.scraper.driver.execute_script("window.scrollTo(0, 20);")
+        self.scraper.scroll_to_bottom()
+        height2 = self.scraper.driver.execute_script("window.scrollTo(0, 20);")
+        self.assertAlmostEqual(height1,height2)
+
+    def test_get_all_links(self):
+        self.scraper.search()
+        self.scraper.accept_form()
+        self.scraper.scroll_to_bottom()
+
+        path1 = os.path.join('/Users/vikasiniperemakumar/Desktop/AiCore/Data_Collection_Pipeline/raw_data')
+        expected_path = os.path.join('/Users/vikasiniperemakumar/Desktop/AiCore/Data_Collection_Pipeline/raw_data')
+        self.assertEqual(expected_path, path1)
+
+    def test_get_all_images(self):
+        self.scraper.get_all_images = True
+        try:
+            print('All images downloaded in a folder')
+            self.assertTrue(self.scraper.get_all_images)
+        except:
+            print('Error downloading the images in a folder.')
+            pass
+
+if __name__ == '__main__':
+    unittest.main(argv = [''],warnings='ignore')
